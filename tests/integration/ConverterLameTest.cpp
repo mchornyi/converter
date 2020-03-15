e7434057df4245bb091c2d9d2e6cf334e691045c
Mykola Chornyi <ext-mykola.chornyi@here.com>
2020-03-15 16:52:12 +0200

    Improve ConverterLameTest.DefaultConvert

--- tests/integration/ConverterLameTest.cpp
+++ tests/integration/ConverterLameTest.cpp
@@ -2,14 +2,43 @@
 
 #include "ConverterLame.h"
 
+#include <fstream>
+#include <sstream>
+
 namespace
 {
 using namespace converter;
 
+long long
+file_size( const std::string& file_path )
+{
+    std::ifstream is( file_path, std::ifstream::binary );
+
+    is.seekg( 0, is.end );
+
+    const auto length = is.tellg( );
+
+    is.close( );
+
+    return length;
+}
+
 TEST( ConverterLameTest, DefaultConvert )
 {
     ConverterLame converter_lame;
-    //converter_lame.convert("lame -q0 -b128 ./res/testcase.wav ./res/testcase.mp3");
-    converter_lame.convert("lame -q0 -b128 testcase.wav testcase.mp3");
+
+    const std::string file_path_out( "./res/testcase.mp3" );
+
+    std::stringstream argumets;
+
+    argumets << "lame -q4 -b160";  // good quality
+    argumets << " ./res/testcase.wav";
+    argumets << " " << file_path_out;
+
+    converter_lame.convert( argumets.str( ) );
+
+    const auto size = file_size( file_path_out );
+    EXPECT_EQ( 12538, size );
+    ASSERT_EQ( 0, remove( file_path_out.c_str( ) ) );
 }
 }  // namespace
